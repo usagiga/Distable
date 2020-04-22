60a1caffd461ac367fa967c8e3ed15d6983238c1
usagiga <usagigawara@gmail.com>
2020-04-23 01:21:43 +0900

    #2 Add EmojiSyncStreamCommand

---
+++ presentation/emoji_sync_stream.go
@@ -0,0 +1,51 @@
+package presentation
+
+import (
+	"context"
+	"github.com/usagiga/distable/application"
+	"github.com/usagiga/distable/entity"
+	"log"
+	"os"
+	"os/signal"
+)
+
+// EmojiSyncStreamCommandImpl is struct implemented `EmojiSyncStreamCommand`.
+type EmojiSyncStreamCommandImpl struct {
+	emojiSyncStreamApplication application.EmojiSyncStreamApplication
+}
+
+// NewEmojiSyncStreamCommand initializes `EmojiSyncStreamCommand`
+func NewEmojiSyncStreamCommand(
+	emojiSyncStreamApplication application.EmojiSyncStreamApplication,
+) EmojiSyncStreamCommand {
+	return &EmojiSyncStreamCommandImpl{
+		emojiSyncStreamApplication: emojiSyncStreamApplication,
+	}
+}
+
+// RunSyncService lets Distable wait updating emojis to sync them among specified servers.
+func (e *EmojiSyncStreamCommandImpl) RunSyncService(servers []entity.ServerContext) {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	errCh := make(chan error)
+
+	// Run
+	go func(){
+		err := e.emojiSyncStreamApplication.WatchUpdatingEmoji(ctx, servers)
+		if err != nil {
+			errCh <- err
+		}
+	}()
+
+	// Wait ^C or to raise errors
+	interrupt := make(chan os.Signal)
+	signal.Notify(interrupt, os.Interrupt)
+	log.Println("Distable(Stream-Mode) is now running... Press ^C to quit")
+
+	select {
+	case <-interrupt:
+		cancel()
+	case err := <-errCh:
+		log.Fatal("Error raised: ", err)
+	}
+}
--- presentation/presentation.go
+++ presentation/presentation.go
@@ -5,6 +5,13 @@ import "github.com/usagiga/distable/entity"
 // EmojiSyncCommand is a view on CLI.
 // It shows results of `EmojiSyncApplications`.
 type EmojiSyncCommand interface {
-	// Sync let Distable sync emoji among specified servers.
+	// Sync lets Distable sync emoji among specified servers now.
 	Sync(servers []entity.ServerContext)
+}
+
+// EmojiSyncStreamCommand is a view on CLI.
+// It shows results of `EmojiSyncStreamApplications`.
+type EmojiSyncStreamCommand interface {
+	// RunSyncService lets Distable wait updating emojis to sync them among specified servers.
+	RunSyncService(servers []entity.ServerContext)
 }
